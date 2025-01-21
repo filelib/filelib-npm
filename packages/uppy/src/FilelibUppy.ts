b55938f8676ff5d8c23c91109aaa70246cdae3f8
JustinMusti <justinmusti@users.noreply.github.com>
2025-01-20 22:56:35 -0500

    uppy package formatting.

--- packages/uppy/src/FilelibUppy.ts
+++ packages/uppy/src/FilelibUppy.ts
@@ -25,7 +25,6 @@ export default class FilelibUppy<M extends Meta, B extends Body> extends BasePlu
         this.type = "uploader"
         this.id = this.opts.id || "Filelib"
         const onError = (err: UploadError) => {
-            console.log("INIT ERROR", err)
             this.uppy.emit("error", err)
             this.opts?.onError(err)
         }
@@ -33,19 +32,16 @@ export default class FilelibUppy<M extends Meta, B extends Body> extends BasePlu
         this.client = new Client({ authKey: opts.authKey, onError })
 
         this.uppy.on("file-removed", (file) => {
-            console.log("REMOVING FILE", file)
             this.client.removeFile({ id: file.id })
         })
 
         this.uppy.on("file-added", (file) => {
             this.addFile(file)
         })
-        console.log("Filelib uploader initialized with opts", opts)
     }
 
     private addFile(file: UppyFile<M, B>) {
         const onProgress = (bytesUploaded: number, bytesTotal: number) => {
-            console.log("Updating progress ", bytesUploaded, bytesTotal, file.progress)
             this.uppy.emit("upload-progress", file, {
                 // uploadStarted: f.progress.uploadStarted ?? 0,
                 uploadStarted: 1,
@@ -60,14 +56,12 @@ export default class FilelibUppy<M extends Meta, B extends Body> extends BasePlu
                 status: 200,
                 body: {} as B
             }
-            console.log("Ayoo", filelibFile)
             this.uppy.emit("upload-success", file, uploadResp)
 
             this.opts?.onSuccess(filelibFile)
         }
 
         const onError: UploaderOpts["onError"] = (fileMeta, err: UploadError) => {
-            console.log("UPLOAD ERROR", file, err)
             this.uppy.emit("upload-error", file, err)
 
             this.opts?.onError(err)
@@ -86,9 +80,6 @@ export default class FilelibUppy<M extends Meta, B extends Body> extends BasePlu
     }
 
     uploadFiles = async (files: UppyFile<M, B>[]): Promise<void> => {
-        console.log("RECEIVED FILES FOR UPLOAD", files)
-        console.log("CLIENT", Client)
-
         if (!this.client.files || this.client.files.length < 1) {
             this.uppy.emit("error", new Error("No Files to upload in Filelib Client."))
             return
@@ -98,7 +89,6 @@ export default class FilelibUppy<M extends Meta, B extends Body> extends BasePlu
     }
 
     #handleUpload = async (fileIDs: string[]) => {
-        console.log("#handleUpload CALLED", fileIDs)
         this.uppy.log("INITIATING UPLOAD")
         if (fileIDs.length === 0) {
             this.uppy.log("No files to upload")
